f5c5bd5b58d5a30674493f71d450b59677368f2b
Frank Anin <frankanin99@gmail.com>
2023-11-06 19:20:59 +0000

    new type struct dog for dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - good boy's details
+ * @name: good boy's name
+ * @age: good boy's age
+ * @owner: lucky owner
+ *
+ * Description: contains introduction for dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* STRUCT_DOG */
