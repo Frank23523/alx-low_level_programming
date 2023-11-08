55483da961ca24cdfcdf7e46ebfe30ea4b8d10d0
Frank Anin <frankanin99@gmail.com>
2023-11-08 21:45:06 +0000

    simple operations functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of the two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of the two numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates remainder of the division of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
