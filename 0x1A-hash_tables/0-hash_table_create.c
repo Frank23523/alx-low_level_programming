23e41e8050878aa469b7a61a3659d196b758e65e
Frank Anin <frankanin99@gmail.com>
2024-02-02 17:07:39 +0000

    creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the array.
+ *
+ * Return: a pointer to the newly created hash table, else NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i = 0;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
+		return (NULL);
+
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
+
+	return (ht);
+}
