178f8d1b3db04af5eefd7331fd042b3d6314aeab
Frank Anin <frankanin99@gmail.com>
2024-02-02 16:44:21 +0000

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,23 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_t *new_table;
+	hash_table_t *ht;
+	unsigned long int i = 0;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
 
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
 		return (NULL);
+	
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
 
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
-
-	return (new_table);
+	return (ht);
 }
